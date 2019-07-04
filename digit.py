34b8362d589f4f63d942fc26918dd497517b0784
edavanhalen <50639181+edavanhalen@users.noreply.github.com>
2019-07-04 12:14:01 +0300

    Add files via upload
    Gives you the number of digits & sum of digits of the number you entered.

---
+++ digit.py
@@ -0,0 +1,12 @@
+count = 0
+sum = 0
+n = int(input("please enter a number.: "))
+
+while n > 0:
+    d = n%10
+    n = n//10
+    count = count + 1 
+    sum += d
+	
+print("Number of digits:",count)
+print("The sum of digits:",sum)
