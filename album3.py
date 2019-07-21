5d01af31a2213cd96f24454916f1f1165077bacb
edavanhalen <edavanhalen@edavanpage>
2019-07-21 20:32:05 +0300

    a

--- album3.py
+++ album3.py
@@ -4,3 +4,4 @@ type(album)
 album['name'] = ['Orchid', 'Morningrise', 'My Arms Your Hearse', 'Still Life', 'Blackwater Park', 'Deliverance', 'Damnation', 'Ghost Reveries', 'Watershed', 'Heritage', 'Pale Communion', 'Sorceress']
 album['year'] = [1995, 1996, 1998, 1999, 2001, 2002, 2003, 2005, 2008, 2011, 2014, 2016]
 album['length'] = ["8 songs: 1 hr 11 min", "6 songs: 1 hr 14 min", "11 songs: 1 hr 2 min", "7 songs: 1 hr 2 min", "9 songs: 1 hr 16 min", "6 songs: 1hr 1 min", "8 songs: 43 min 6 sec", "8 songs: 1 hr 6 min", "10 songs: 1 hr 11 min", "10 songs: 56 min 52 sec", "8 songs: 55 min 42 sec", "16 songs: 1 hr 30 min"]
+print(album)
