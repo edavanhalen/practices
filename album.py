fa2d80a49ba788ff61b506a082ef3dbaeaa25bc1
edavanhalen <edavanhalen@edavanpage>
2019-07-21 19:46:28 +0300

    album

--- album.py
+++ album.py
@@ -1,6 +1,6 @@
 album = {
-    "name": "Orchid","Morningrise","My Arms Your Hearse","Still Life","Blackwater Park","Deliverance","Damnation","Ghost Reveries","Watershed","Heritage","Pale Communion","Sorceress"
-    "year": 1995,1996,1998,1999,2001,2002,2003,2005,2008,2011,2014,2016
-    "length": "8 songs: 1 hr 11 min", "6 songs: 1 hr 14 min", "11 songs: 1 hr 2 min", "7 songs: 1 hr 2 min","9 songs: 1 hr 16 min","6 songs: 1hr 1 min","8 songs: 43 min 6 sec","8 songs: 1 hr 6 min","10 songs: 1 hr 11 min","10 songs: 56 min 52 sec","8 songs: 55 min 42 sec","16 songs: 1 hr 30 min"
+    "name": "Orchid", "Morningrise", "My Arms Your Hearse", "Still Life", "Blackwater Park", "Deliverance", "Damnation", "Ghost Reveries", "Watershed", "Heritage", "Pale Communion", "Sorceress"
+    "year": 1995, 1996, 1998, 1999, 2001, 2002, 2003, 2005, 2008, 2011, 2014, 2016
+    "length": "8 songs: 1 hr 11 min", "6 songs: 1 hr 14 min", "11 songs: 1 hr 2 min", "7 songs: 1 hr 2 min", "9 songs: 1 hr 16 min", "6 songs: 1hr 1 min", "8 songs: 43 min 6 sec", "8 songs: 1 hr 6 min", "10 songs: 1 hr 11 min", "10 songs: 56 min 52 sec", "8 songs: 55 min 42 sec", "16 songs: 1 hr 30 min"
 }
 print(album)
