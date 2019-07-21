65ac27da042587712f4a02a2f96f25ca93ba987e
edavanhalen <edavanhalen@edavanpage>
2019-07-21 21:04:18 +0300

    a

--- album2.py
+++ album2.py
@@ -1,5 +1,8 @@
 album = {Orchid: {'Year': 1995, 'Songs': '8 songs', 'Length': '1 hr 11 min'}, \
 Morningrise: {'Year': 1996, 'Songs': '6 songs', 'Length': '1 hr 14 min'}}
 
-for y, s, l in album.items():
-    print("\nName:", y, s, l)
+for name, info in album.items():
+    print("\nName:", info)
+
+    for key in info:
+        print (key + ':', info[key])
