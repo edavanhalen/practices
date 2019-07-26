98b82bd93afe9d0e0ade110ac379f50ad3f960b2
edavanhalen <edavanhalen@edavanpage>
2019-07-26 23:15:52 +0300

    a

---
+++ albumfunc.py
@@ -0,0 +1,9 @@
+def album( name, year, tracks, length ):
+    print "Name: ", name
+    print "Year: ", year
+    print "Tracks: ", tracks
+    print "Length": , length
+    return;
+
+album( 'Orchid', '1995', '8', '1 hr 11 min')
+album( 'Morningrise', '1996', '6', '1 hr 14 min')    
