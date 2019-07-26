ff968c395b7428b79f6fe11e7ce4c6a6ce820991
Ali Kutlu Durşen <alidursen@gmail.com>
2019-07-26 23:45:28 +0300

    returns a dict

--- albumfunc.py
+++ albumfunc.py
@@ -1,10 +1,10 @@
 def album( name, year, tracks, length ):
-    print ("Name: ", name)
-    print ("Year: ", year)
-    print ("Tracks: ", tracks)
-    print ("Length: ", length)
-    print ("__________________")
-    return;
+    return {
+        "Name": name,
+        "Year": year,
+        "Tracks": tracks,
+         "Length": length
+        }
 
 album( 'Orchid', '1995', '8 tracks', '1 hr 11 min')
 album( 'Morningrise', '1996', '6 tracks', '1 hr 14 min')
