fae0617b3cbc778c1436088a8c88d6bf2453d796
edavanhalen <edavanhalen@edavanpage>
2019-07-26 23:23:25 +0300

    a

--- albumfunc.py
+++ albumfunc.py
@@ -5,5 +5,15 @@ def album( name, year, tracks, length ):
     print ("Length: ", length)
     return;
 
-album( 'Orchid', '1995', '8', '1 hr 11 min')
-album( 'Morningrise', '1996', '6', '1 hr 14 min')    
+album( 'Orchid', '1995', '8 tracks', '1 hr 11 min')
+album( 'Morningrise', '1996', '6 tracks', '1 hr 14 min')
+album( 'My Arms Your Hearse', '1998', '11 tracks', '1 hr 2 min')
+album( 'Still Life', '1999', '7 tracks', '1 hr 2 min')
+album( 'Blackwater Park', '2000', '9 tracks', ' hr 16 min')
+album( 'Deliverance', '2002', '6 tracks', '1 hr 1 min' )
+album( 'Damnation', '2003','8 tracks', '43 min')
+album( 'Ghost Reveries', '2005', '8 tracks', '1 hr 6 min')
+album( 'Watershed', '2008', '10 tracks', '1 hr 11 min')
+album( 'Heritage', '2011', '10 tracks', '56 min')
+album( 'Pale Communion', '2014', '8 tracks', '55 min')
+album( 'Sorceress', '2016', '16 tracks', '1 hr 30 min')    
